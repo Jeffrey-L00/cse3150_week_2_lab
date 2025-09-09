5ea58afa7a6044e63c8065d5194da45229b8584d
confusionsvkda <110402117+confusionsvkda@users.noreply.github.com>
2025-09-09 16:34:08 -0400

    edited main.cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,15 @@
 using std::cout, std::cin, std::endl, std::string, std::getline;
 
 int main() {
-	string fullName;
-	std::getline(cin, fullName);
+	string fullName; //initalizes fullName variable as a string
+	std::getline(cin, fullName); //grabs the full line from input instead of stopping at a whitespace
 
 	string email;
-	cin >> email;
+	cin >> email; //reads input but stops at whitespace, good for single words like a email address but not a sentence
+
 	
-	string* firstName = new std::string;
-	string* lastName = new std::string;
+	string* firstName = new std::string; //dynamically allocates two string objects onto heap. pointer to std::string, holding the memory address. parseName expects memory addresses to write directly to the locations. ('new' allocates memory on the heap)
+	string* lastName = new std::string; 
 
 	StringUtils::parseName(fullName, firstName, lastName);
 	string userName = StringUtils::getUsername(email);
@@ -22,8 +23,8 @@ int main() {
 	cout << "Last Name:" << *lastName << endl; 
 	cout << "Username:" << userName << endl;
 	
-	delete firstName;
-	delete lastName;
+	delete[] firstName;
+	delete[] lastName;
 	return 0;
 }	
 
